f2e195d28e8d00211431ea2290113bb37a42fcd7
KumaStar <aryanbhatt2982@gmail.com>
2026-01-18 12:56:49 +0530

    added menu and button promesh

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,21 +2,26 @@ using UnityEngine;
 using System;
 public class Player : MonoBehaviour
 {
-    private float _JumpEndTime;
-
-    [SerializeField] private float _HorizontelVelocity = 5f;
+    
+    [SerializeField] private float _maxHorizontalSpeed = 5f;
     [SerializeField] private float _JumpVelocity = 5f;
     [SerializeField] private float _JumpDuration = 0.5f;
     [SerializeField] private Sprite _JumpSprite;
     [SerializeField] private LayerMask _layerMask;
     [SerializeField] private float _footOffset = 0.1f;
+    [SerializeField] private float _groundAcceleration = 10f;
+    [SerializeField] private float _snowAcceleration = 1f;
+
     private float _horizontal;
     private int _jumpRemaining;
+    private float _JumpEndTime;
     private AudioSource _audioSource;
     private SpriteRenderer _spriteRenderer;
     private Animator _animator;
 
+    
     public bool IsGrounded;
+    public bool IsOnSnow;
 
     private void Awake()
     {
@@ -46,8 +51,7 @@ public class Player : MonoBehaviour
         updateGrounding();
 
         // Movement
-        _horizontal = Input.GetAxis("Horizontal");
-        Debug.Log(_horizontal);
+        var horizontalInput = Input.GetAxis("Horizontal");
         Rigidbody2D rb = GetComponent<Rigidbody2D>();
         var vertical = rb.linearVelocity.y;
 
@@ -62,7 +66,10 @@ public class Player : MonoBehaviour
         {
             vertical = _JumpVelocity;
         }
-        _horizontal *= _HorizontelVelocity;
+        
+        var desired_horizontal = horizontalInput * _maxHorizontalSpeed;
+        var acceleration = IsOnSnow ? _snowAcceleration : _groundAcceleration;
+        _horizontal = Mathf.Lerp(_horizontal, desired_horizontal, Time.deltaTime * acceleration);
         rb.linearVelocity = new Vector2(_horizontal, vertical);
         updateSprite();
     }
@@ -70,28 +77,36 @@ public class Player : MonoBehaviour
     private void updateGrounding()
     {
         IsGrounded = false;
+        IsOnSnow = false;
+
         // Ground check
         Vector2 origin = new Vector2(transform.position.x, transform.position.y - _spriteRenderer.bounds.extents.y);
         RaycastHit2D hit = Physics2D.Raycast(origin, Vector2.down, 0.1f, _layerMask);
 
         if (hit.collider)
+        {
             IsGrounded = true;
-
-        // Left foot
-        origin = new Vector2(transform.position.x - _footOffset, transform.position.y - _spriteRenderer.bounds.extents.y);
+            IsOnSnow = hit.collider.CompareTag("Snow");
+        }
+            // Left foot
+            origin = new Vector2(transform.position.x - _footOffset, transform.position.y - _spriteRenderer.bounds.extents.y);
         hit = Physics2D.Raycast(origin, Vector2.down, 0.1f, _layerMask);
 
         if (hit.collider)
+        {
             IsGrounded = true;
-
+            IsOnSnow = hit.collider.CompareTag("Snow");
+        }
         // Right foot
         origin = new Vector2(transform.position.x + _footOffset, transform.position.y - _spriteRenderer.bounds.extents.y);
         hit = Physics2D.Raycast(origin, Vector2.down, 0.1f, _layerMask);
 
         if (hit.collider)
+        {
             IsGrounded = true;
-
-        if(IsGrounded && GetComponent<Rigidbody2D>().linearVelocity.y <= 0)
+            IsOnSnow = hit.collider.CompareTag("Snow");
+        }
+        if (IsGrounded && GetComponent<Rigidbody2D>().linearVelocity.y <= 0)
         {
             _jumpRemaining = 2;
         }
